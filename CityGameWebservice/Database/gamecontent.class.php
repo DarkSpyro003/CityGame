43c7d689f9db2ed79623d8cdd14b0c9aeb6f436d
Christina Korosec <christina.korosec1@gmail.com>
2015-01-14 14:09:53 +0100

    GameContent should have a title (such as city name) to identify it!

--- CityGameWebservice/Database/gamecontent.class.php
+++ CityGameWebservice/Database/gamecontent.class.php
@@ -2,11 +2,22 @@
 require_once 'question.class.php';
 class GameContent
 {
+	private $title;
 	private $questionList = array();
 	
-	public GameContent($questionList)
+	public GameContent($title)
 	{
-		$this->questionList = $questionList;
+		$this->title = $title;
+	}
+	
+	public getTitle()
+	{
+		return $this->title;
+	}
+	
+	public setTitle($title)
+	{
+		$this->title = $title;
 	}
 }
 ?>
