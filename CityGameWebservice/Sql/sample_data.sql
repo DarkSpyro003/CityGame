88a57067086ae4a673fc1357dd66b97756cdfc8f
Christina Korosec <christina.korosec1@gmail.com>
2015-01-14 15:53:34 +0100

    Sample question data for the database

---
+++ CityGameWebservice/Sql/sample_data.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2015 at 03:52 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `citygame`
+--
+
+--
+-- Dumping data for table `gamecontent`
+--
+
+INSERT INTO `gamecontent` (`id`, `title`) VALUES
+(1, 'A list of testquestions');
+
+--
+-- Dumping data for table `multi_answer`
+--
+
+INSERT INTO `multi_answer` (`questionId`, `choiceId`, `answer`) VALUES
+(1, 0, 'yes'),
+(1, 1, 'no');
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `type`, `question`, `text_answer`, `multi_answer`, `extraInfo`, `content_url`, `gamecontentId`, `latitude`, `longitude`) VALUES
+(1, 0, 'Is this a testquestion?', NULL, 0, 'Yes, this was a testquestion, since it''s in the test gamecontent!', 'http://public.ds003.info/imghost/testcontent.png', 1, 50.9167, 5.3333),
+(2, 1, 'This is another testquestion. To answer this question correctly, fill out the opposite word of ''no'' in the answer.', 'yes', NULL, 'And this was the second testquestion, this one in plain text!', 'http://public.ds003.info/imghost/testcontent.png', 1, 50.9368583, 5.3483892);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
