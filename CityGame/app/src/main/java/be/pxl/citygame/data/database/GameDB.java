0d3c20df0d2dce1b81466ecb711a7fff1be1ebe1
Christina Korosec <christina.korosec1@gmail.com>
2015-02-03 18:38:19 +0100

    Preparation for local GameDB

---
+++ CityGame/app/src/main/java/be/pxl/citygame/data/database/GameDB.java
@@ -0,0 +1,27 @@
+package be.pxl.citygame.data.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Christina on 3/02/2015.
+ */
+public class GameDB {
+
+    public static final String DB_NAME = "game_content_db";
+    public static final int DB_VER = 1;
+
+    public static final class Games implements BaseColumns {
+        public static final String TABLE_NAME = "games";
+        public static final String COL_GID = "gid";
+        public static final String COL_USERNAME = "title";
+        public static final String COL_COMPLETED = "completed";
+    }
+
+    public static final class Questions implements BaseColumns {
+        public static final String TABLE_NAME = "questions";
+        public static final String COL_QID = "qid";
+        public static final String COL_GID = "gid";
+        public static final String COL_ANSWERED = "answered";
+        public static final String COL_ANSWERED_CORRECT = "answeredCorrect";
+    }
+}
---
+++ CityGame/app/src/main/java/be/pxl/citygame/data/database/GameDbHelper.java
@@ -0,0 +1,41 @@
+package be.pxl.citygame.data.database;
+
+import android.app.Application;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Christina on 3/02/2015.
+ */
+public class GameDbHelper extends SQLiteOpenHelper {
+
+    public GameDbHelper(Context ctx) {
+        super(ctx, GameDB.DB_NAME, null, GameDB.DB_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sqlCreate = "CREATE TABLE " + GameDB.Games.TABLE_NAME + " (" +
+                GameDB.Games._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                GameDB.Games.COL_GID + " INTEGER UNIQUE, " +
+                GameDB.Games.COL_USERNAME + " TEXT, " +
+                GameDB.Games.COL_COMPLETED + " INTEGER DEFAULT 0); " +
+                "CREATE TABLE " + GameDB.Questions.TABLE_NAME + " (" +
+                GameDB.Questions._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                GameDB.Questions.COL_QID + " INTEGER, " +
+                GameDB.Questions.COL_GID + " INTEGER, " +
+                GameDB.Questions.COL_ANSWERED + " INTEGER, " +
+                GameDB.Questions.COL_ANSWERED_CORRECT + " INTEGER, " +
+                "CONSTRAINT unq UNIQUE (" + GameDB.Questions.COL_QID + ", " + GameDB.Questions.COL_GID + "));";
+
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // No upgrades yet
+    }
+}
