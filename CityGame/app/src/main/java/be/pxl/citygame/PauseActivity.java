7dbbaffdb2486ec73e3e7bb3864ec2a0ec08aa1f
Jorne De Smedt <jornedesmedt@hotmail.com>
2015-02-04 15:30:11 +0100

    Added PauseActivity.java

---
+++ CityGame/app/src/main/java/be/pxl/citygame/PauseActivity.java
@@ -0,0 +1,51 @@
+package be.pxl.citygame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PauseActivity extends ActionBarActivity {
+
+    private int currGame;
+    private int currQuestion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_location);
+
+        ((CityGameApplication)getApplicationContext()).setActivity(this);
+
+        Intent intent = getIntent();
+        currGame = intent.getIntExtra("gameId", 0);
+        currQuestion = intent.getIntExtra("questionId", 0);
+
+        PauseFragment mapfrag = (PauseFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_pause);
+        mapfrag.setData(currGame, currQuestion);
+        mapfrag.showPOIS("restaurant");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_next_location, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
