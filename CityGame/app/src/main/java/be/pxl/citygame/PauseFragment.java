46927a225067ae8bb74e84b078404319707d30dd
Jorne De Smedt <jornedesmedt@hotmail.com>
2015-02-04 15:15:12 +0100

    Removed TODO from PauseFragment

--- CityGame/app/src/main/java/be/pxl/citygame/PauseFragment.java
+++ CityGame/app/src/main/java/be/pxl/citygame/PauseFragment.java
@@ -110,7 +110,6 @@ public class PauseFragment extends Fragment implements ILocationRequest {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
-    // TODO: move OpenData Restaurants to its own Activity for different style of map controls (such as not locking onto gps marker)
 
     public  void showPOIS(String tag)
     {
