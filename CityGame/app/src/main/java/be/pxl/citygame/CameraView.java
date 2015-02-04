bf8fcda39f1436680bfa1e90919f1e72d84748e9
LorenzJ <lorenz_jolling@hotmail.com>
2015-02-04 13:54:33 +0100

    Camera orientation WIP.

--- CityGame/app/src/main/java/be/pxl/citygame/CameraView.java
+++ CityGame/app/src/main/java/be/pxl/citygame/CameraView.java
@@ -1,9 +1,12 @@
 package be.pxl.citygame;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Point;
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import java.io.IOException;
 
@@ -28,6 +31,21 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
         try {
             theCamera.setPreviewDisplay(holder);
             theCamera.startPreview();
+            WindowManager windowManager = (WindowManager)getContext().getSystemService(Context.WINDOW_SERVICE);
+            Point size = new Point();
+            windowManager.getDefaultDisplay().getSize(size);
+            float aspectRatio = size.x / size.y;
+
+            switch (getResources().getConfiguration().orientation) {
+                case Configuration.ORIENTATION_LANDSCAPE:
+                    holder.setFixedSize((int)((float)size.x * (0.9) * aspectRatio), size.y);
+                    break;
+                case Configuration.ORIENTATION_PORTRAIT:
+                    holder.setFixedSize(size.x, (int) ((float) size.y / (0.9) * aspectRatio));
+                    break;
+            }
+
+
         } catch (IOException e) {
         }
     }
