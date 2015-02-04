e47930885cb84c420f68fe5bfd5adb1228b33b10
Jorne De Smedt <jornedesmedt@hotmail.com>
2015-02-04 15:19:58 +0100

    Removed All traces of ShowPOIS and now unused imports from MapFragment

--- CityGame/app/src/main/java/be/pxl/citygame/MapFragment.java
+++ CityGame/app/src/main/java/be/pxl/citygame/MapFragment.java
@@ -1,37 +1,22 @@
 package be.pxl.citygame;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;;
 import android.location.Location;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Debug;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.osmdroid.bonuspack.location.NominatimPOIProvider;
-import org.osmdroid.bonuspack.location.POI;
-import org.osmdroid.bonuspack.overlays.FolderOverlay;
-import org.osmdroid.bonuspack.overlays.Marker;
+
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.DirectedLocationOverlay;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.OverlayItem;
-import org.osmdroid.views.overlay.SimpleLocationOverlay;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import be.pxl.citygame.gps.ILocationRequest;
@@ -117,58 +102,6 @@ public class MapFragment extends Fragment implements ILocationRequest {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);
-    }
-
-
-    public  void showPOIS(String tag)
-    {
-        AsyncTask poiTask = new GetPoi().execute(tag);
-    }
-
-    public void onCallback(ArrayList<POI> pois)
-    {
-        FolderOverlay poiMarkers = new FolderOverlay(getActivity());
-        mapView.getOverlays().add(poiMarkers);
-
-        Drawable poiIcon = getResources().getDrawable(R.drawable.restaurant);
-        if(pois == null)
-        {
-            Log.d("POI", "pois is null");
-        }
-        for(POI poi : pois)
-        {
-            Marker poiMarker = new Marker(mapView);
-            poiMarker.setTitle(poi.mType);
-            poiMarker.setSnippet(poi.mDescription);
-            poiMarker.setPosition(poi.mLocation);
-            poiMarker.setIcon(poiIcon);
-            Log.d("POI", poi.mDescription);
-            /*if(poi.mThumbnail != null)
-            {
-                poiItem.setImage(new BitmapDrawable(poi.mThumbnail));
-            }*/
-            poiMarkers.add(poiMarker);
-        }
-    }
-
-    private class GetPoi extends AsyncTask<String, Void, ArrayList<POI>> {
-
-        @Override
-        protected ArrayList<POI> doInBackground(String... params) {
-            String tag = params[0];
-            NominatimPOIProvider poiProvider = new NominatimPOIProvider();
-            LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            GeoPoint currentPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-            ArrayList<POI> pois = poiProvider.getPOICloseTo(currentPoint, tag, 50, 0.1);
-
-            return pois;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<POI> pois) {
-            onCallback(pois);
-        }
     }
 
 }
