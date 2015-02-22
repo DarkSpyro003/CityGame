05e30c1d768a9369ad99234767496e7b30af2069
Christina Korosec <christina.korosec1@gmail.com>
2015-02-22 19:39:03 +0100

    InfoActivity now correctly configures ImageFragment
    Have to display the correct video/image in the ImageFragment!

--- CityGame/app/src/main/java/be/pxl/citygame/InfoActivity.java
+++ CityGame/app/src/main/java/be/pxl/citygame/InfoActivity.java
@@ -20,7 +20,9 @@ public class InfoActivity extends ActionBarActivity {
 
         Intent intent = getIntent();
         InfoFragment frag = (InfoFragment) getSupportFragmentManager().findFragmentById(R.id.infoFragment);
+        ImageFragment imgFrag = (ImageFragment) getSupportFragmentManager().findFragmentById(R.id.imageFragment);
         frag.setData(intent.getIntExtra("gameId", 0), intent.getIntExtra("questionId", 0));
+        imgFrag.setData(intent.getIntExtra("gameId", 0), intent.getIntExtra("questionId", 0));
     }
 
 
