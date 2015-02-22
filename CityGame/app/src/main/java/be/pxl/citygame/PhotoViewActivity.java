2b9dbca42b1f0de22a1a2eb720c82739a32bb64c
Christina Korosec <christina.korosec1@gmail.com>
2015-02-22 19:48:29 +0100

    Don't create an options menu on PhotoViewActivity

--- CityGame/app/src/main/java/be/pxl/citygame/PhotoViewActivity.java
+++ CityGame/app/src/main/java/be/pxl/citygame/PhotoViewActivity.java
@@ -26,27 +26,4 @@ public class PhotoViewActivity extends ActionBarActivity {
         ImageView image = (ImageView) findViewById(R.id.iv_photo);
         image.setImageURI(uri);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_photo_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
